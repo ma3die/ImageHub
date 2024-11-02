6e4e297f11daebb25d230c2b2c51b2035d4b9bc2
Alex <aatevs@gmail.com>
2024-11-02 20:43:42 +0300

    Image model

---
+++ backend/apps/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Image(models.Model):
+    """Модель изображений"""
+    name = models.CharField(max_length=255, verbose_name="Название изображения")
+    file_path = models.CharField(max_length=255, verbose_name="Путь к файлу")
+    upload_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата загрузки")
+    resolution = models.CharField(max_length=255, verbose_name="Разрешение")
+    size = models.IntegerField(verbose_name="Размер файла")
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
+        ordering = ["-upload_date"]
+
+    def __str__(self):
+        return self.name
+
+
